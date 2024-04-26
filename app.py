95da4a7405d0ff688813ae503987b6121f72b87e
N9oBSi <abed.salloura08@gmail.com>
2024-04-26 10:35:39 +0300

    feat: Add events visualization

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from controllers.home_controller import home_page, privacy
+from controllers.home_controller import home_page, privacy, events
 
 # app = Flask(__name__)
 
@@ -7,6 +7,7 @@ app = Flask(__name__, static_folder='static')
 
 app.add_url_rule('/', 'home', home_page)
 app.add_url_rule('/privacy', 'privacy', privacy)
+app.add_url_rule('/events', 'events', events)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- controllers/home_controller.py
+++ controllers/home_controller.py
@@ -3,6 +3,13 @@ from flask import render_template
 def home_page():
     return render_template('home.html')
 
+def events():
+    events = [
+        {'id': 1, 'text': "Doctor's appointment", 'date': '2024-04-30'},
+        {'id': 2, 'text': "Team meeting at work", 'date': '2024-05-01'},
+        {'id': 3, 'text': "Friend's wedding", 'date': '2024-05-15'}
+    ]
+    return render_template('events.html', events=events)
 
 def privacy():
     data = {
