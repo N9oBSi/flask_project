4774c67919a7a02e324f659d318b3a195e37e3ca
N9oBSi <abed.salloura08@gmail.com>
2024-05-16 10:11:55 +0300

    feat: Add new task in database

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template
-from controllers.home_controller import home_page, privacy, events
-
-# app = Flask(__name__)
+from flask_mysqldb import MySQL
 
 app = Flask(__name__, static_folder='static')
+
+# Configure MySQL Connection
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'events_scheduler'
+mysql = MySQL(app)
+
+# Import routes after initializing the app and mysql
+from controllers.home_controller import home_page, privacy, events, add_event
 
 app.add_url_rule('/', 'home', home_page)
 app.add_url_rule('/privacy', 'privacy', privacy)
 app.add_url_rule('/events', 'events', events)
+app.add_url_rule('/events/new', 'new_event', add_event, add_event, methods=['GET', 'POST'])
 
 if __name__ == "__main__":
     app.run(debug=True)
--- controllers/home_controller.py
+++ controllers/home_controller.py
@@ -1,15 +1,34 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from flask import current_app as app
 
 def home_page():
     return render_template('home.html')
 
 def events():
-    events = [
-        {'id': 1, 'text': "Doctor's appointment", 'date': '2024-04-30'},
-        {'id': 2, 'text': "Team meeting at work", 'date': '2024-05-01'},
-        {'id': 3, 'text': "Friend's wedding", 'date': '2024-05-15'}
-    ]
-    return render_template('events.html', events=events)
+    # Importing mysql here to avoid circular import
+    from app import mysql
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT id, text, date FROM tasks ORDER BY date")
+    events = cur.fetchall()
+    cur.close()
+    event_list = [{'id': event[0], 'text': event[1], 'date': event[2].isoformat()} for event in events]
+
+    return render_template('events.html', events=event_list)
+
+def add_event():
+    from app import mysql
+    if request.method == 'POST':
+        event_text = request.form['text']
+        event_date = request.form['date']
+        
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO tasks (text, date) VALUES (%s, %s)", (event_text, event_date))
+        mysql.connection.commit()
+        cur.close()
+        
+        return redirect(url_for('events'))
+    
+    return render_template('add_event.html')
 
 def privacy():
     data = {
